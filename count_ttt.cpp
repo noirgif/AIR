aaa9f3689a075e52b804b020a35aeb0e60805ca0
noirgif <nomaru@outlook.com>
2018-04-10 14:45:37 +0800

    Forsaken bit, same result

--- count_ttt.cpp
+++ count_ttt.cpp
@@ -4,80 +4,46 @@
  * Empty:0 X:1 O:2
  */
 #include <iostream>
+#include <vector>
 
-using board = int;
+using board = std::vector<short>;
 
-// masks
-const board row = 077;
-const board col = 030303;
-const board diag[] = {0601403,0031460};
-const board odd = 0x55555;
-const board even = 0xaaaaa;
+int get_lines(const board &x, int c)
+{
+	int ans = 0;
+	for (int i = 0; i < 3; ++i)
+	{
+		// columns
+		if (x[0 + i] == c && x[3 + i] == c && x[6 + i] == c)
+			++ans;
+		// rows
+		if (x[0 + i * 3] == c && x[1 + i * 3] == c && x[2 + i * 3] == c)
+			++ans;
+	}
+	if (x[0] == c && x[4] == c && x[8] == c)
+		++ans;
+	if (x[2] == c && x[4] == c && x[6] == c)
+		++ans;
+	return ans;
+}
+
+int get_count(const board& x, int c)
+{
+	int ans = 0;
+	for (auto i : x)
+		ans += (i == c);
+	return ans;
+}
 
 // see if it is a reachable state
 bool reachable(const board &x)
 {
-	
-		// winner of the game, if any
-	int win = 0;
+	int xcount = get_count(x, 1);
+	int ocount = get_count(x, 2);
+	int x_lines = get_lines(x, 1);
+	int o_lines = get_lines(x, 2);
 
-	// one cannot win twice, nor after his opponents' victory
-	for (int i = 0; i < 3; ++i)
-	{
-		int j = row & (x >> (i * 6));
-		j &= (j >> 2);
-		j &= (j >> 2) & 3;
-		if (j)
-		if (!win)
-			win = j;	
-		else
-			return false;
-	}
-
-	for (int i = 0; i < 3; ++i)
-	{
-		int j = col & (x >> (i * 2));
-		j &= (j >> 6);
-		j &= (j >> 6) & 3;
-		if (j)
-		if (!win)
-			win = j;
-		else
-			return false;
-	}
-	
-	for (int i = 0; i < 2; ++i)
-	{
-		int j = diag[i] & x;
-		j &= (j >> 8 - 4 * i);
-		j &= (j >> 8 - 4 * i);
-		j = (j >> 4*i) & 3;
-		if (j)
-		if (!win)
-			win = j;
-		else
-			return false;
-	}
-	
-	// check count(X) - count(O) = {0,1}
-	int xcount = 0;
-	for (int i = 0; i < 9; ++i)
-	{
-		// tmp is the piece on the tile
-		int tmp = (x >> i * 2) & 3;
-		// if tmp == 2 then -1 else add tmp
-		xcount += ((tmp == 2) ? -1 : tmp);
-	}
-
-	// if x win, win = 1, xcount = 1, xor -> 0
-	// if o win, win = 2, xcount = 0, xor -> 2
-	switch (win)
-	{
-		case 1: return xcount == 1;
-		case 2: return xcount == 0;
-		default:
-				return xcount == 1 | xcount == 0;
-	}
+	return x_lines + o_lines <= 1 && xcount >= ocount + x_lines && xcount <= ocount + 1 - o_lines;
 }
 
 int main()
@@ -89,12 +55,11 @@ int main()
 	// convert the number to a board
 	for (int i = 0; i < total; ++i)
 	{
-		board tb = 0;
+		board tb;
 		auto cp = i;
 		for (int j = 0; j < 9; ++j)
 		{
-			tb <<= 2;
-			tb |= cp % 3;
+			tb.push_back(cp % 3);
 			cp /= 3;
 		}
 		ans += reachable(tb);
